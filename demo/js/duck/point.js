12bd717e2682d0cf0b62101d056ee3e7b95ce771
Jay Jagpal <jay@strangemother.com>
2015-05-16 22:04:22 +0100

    Simplified point example

--- demo/js/duck/point.js
+++ demo/js/duck/point.js
@@ -1,20 +1,12 @@
 var point = function(o){
-	var np = new o.NullPoint(10,10)
+	var np = new o.NullPoint(50,50)
 	var p = new o.Point(np)
 	this.children.push(p)
 }
 
-var line = function(o) {
-	var a = new o.NullPoint(10,10)
-	var b = new o.NullPoint(10,40)
-	l = new o.Line(a, b)
-	this.children.push(l)
-}
-
 var ready = function(){
-	var canvas = new cotton.duck.objects.Surface('main')
-	canvas.load(['NullPoint', 'Point'], point)
-	canvas.load(['NullPoint', 'Line'], line)
+	var canvas = new cotton.duck.objects.Surface('main');
+	canvas.load(['Point', 'NullPoint'], point);
 };
 
 cotton.ready(ready)
