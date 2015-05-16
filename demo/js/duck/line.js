a9f23362ee8400d7347e1a5f9330578744e7dddf
Jay Jagpal <jay@strangemother.com>
2015-05-16 22:04:13 +0100

    Line example

---
+++ demo/js/duck/line.js
@@ -0,0 +1,20 @@
+var NullPoint;
+var _p = function(x,y) {
+    return (new NullPoint(x,y));
+}
+
+var line = function(o) {
+    l = new o.Line( _p(50,50), _p(50,150) )
+    this.children.push(l)
+}
+
+var ready = function(){
+    var canvas = new cotton.duck.objects.Surface('main');
+    
+    canvas.load(['NullPoint'], function(o){
+        NullPoint = o.NullPoint;
+        canvas.load(['Line'], line);
+    });
+};
+
+cotton.ready(ready)
