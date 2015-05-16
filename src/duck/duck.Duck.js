35cc3cdfd970a60c87ebf5c23bbeb2a315196adf
Jay Jagpal <jay@strangemother.com>
2015-05-16 22:07:04 +0100

    Use internal cache rather than global scope

--- src/duck/duck.Duck.js
+++ src/duck/duck.Duck.js
@@ -8,20 +8,21 @@ as layers and applying those layers to a stack.
 	var listen = window['addEventListener'] ? window.addEventListener: window.attachEvent
 		, scripts = document.getElementsByTagName("script")
 		, script = scripts[scripts.length - 1]
-		, core, spindle, utils, space
+		, core, spindle, utils, space, cache
 		//, BaseClass, Layer, Clock, CanvasClock, DisplayObject
 		, self = this
 		;
 
 	var setup = function(){
-		console.log('Write duck')
-		core.duck.Duck = Layer;
-		core.duck.objects.Clock = Clock;
-		core.duck.objects.Layer = Layer;
-		core.duck.objects.CanvasClock = CanvasClock;
-		core.duck.objects.DisplayObject = DisplayObject;
-		core.duck.objects.Canvas = Canvas;
-		core.duck.objects.Surface = Surface;
+		console.log('Write duck');
+
+		core.duck.Duck = cache.Layer;
+		core.duck.objects.Clock = cache.Clock;
+		core.duck.objects.Layer = cache.Layer;
+		core.duck.objects.CanvasClock = cache.CanvasClock;
+		core.duck.objects.DisplayObject = cache.DisplayObject;
+		core.duck.objects.Canvas = cache.Canvas;
+		core.duck.objects.Surface = cache.Surface;
 
 	}
 
@@ -39,9 +40,11 @@ as layers and applying those layers to a stack.
 			spindle = event.detail.spindle;
 			utils 	= event.detail.utils;
 			space   = core[config.name];
+			cache 	= core.cache;
 	 		self.core = core
 	 		self.spindle = spindle
 	 		self.utils = utils
+
 	 		setup()
 	 		core.ready(ready);
 	 		spindle.bootups[config.name].done()
