7c957f8fe69cba1ef5776a24f8718c0f014e1258
Jay Jagpal <jay@strangemother.com>
2015-05-17 03:07:05 +0100

    reference clean data API

--- src/duck/objects/Line.js
+++ src/duck/objects/Line.js
@@ -10,9 +10,9 @@ cotton.ready(function(){
 				this.constructor.$super.apply(this, arguments);
 			}
 			
-			, init: function(pointA, pointB){
-				this.point = pointA
-				this.points = [pointA, pointB]
+			, init: function(points, data){
+				this.point = points[0]
+				this.points = points
 
 				this.width = 3
 				this.dashOffset = 0
@@ -20,21 +20,34 @@ cotton.ready(function(){
 			}
 
 			, step: function(context, data){
-				data.points = this.points;
-				data.lineWidth = this.width;
-				data.dashOffset = this.dashOffset;
-				data.join = this.join;
-				data.cap = this.cap;
+				var ret = {};
+				ret.points 		= this.points;
+				ret.lineWidth 	= this.width;
+				ret.dashOffset 	= this.dashOffset;
+				ret.join 		= this.join;
+				ret.cap 		= this.cap;
+				
+				if( ret.dashSpace > 0 ) {
+					ret.lineDash = [ret.dashSpace, ret.dashWidth];
+				}
+
+				return ret;
 			}
+			
 			, draw: function(ctx, data){
 				ctx.lineCap = data.cap;
 				ctx.lineWidth = data.width;
 				ctx.lineDashOffset = data.dashOffset;
 				ctx.lineJoin = data.join;
-				ctx.setLineDash([1,3])
+				data.lineDash && ctx.setLineDash(data.lineDash)
 				ctx.beginPath();
 				ctx.moveTo(data.points[0].x, data.points[0].y)
 				ctx.lineTo(data.points[1].x, data.points[1].y)
+				
+				for(var i=0;i<data.points.length; i++) {
+					ctx.lineTo(data.points[i].x, data.points[i].y)	
+				};
+
 		    	ctx.stroke();
 		    	// ctx.endPath();
 			}
@@ -46,5 +59,7 @@ cotton.ready(function(){
 		, join: ['round', 'bevel', 'miter']
 		, width: Number
 		, dashOffset: Number
+		// space between dash
+		, dashSpace: 0
 	}
 })
